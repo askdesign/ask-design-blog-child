52bea5b1e147b6d44863cf7ff89a868226283abb
Anne S. Katzeff <anne@askdesign.biz>
2026-07-31 17:35:57 -0400

    Template parts
    These templates were generated when I tried to create my own sync'd pattern.
    Not sure why.

---
+++ patterns/ask-photo-blog-posts.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: ASK photo blog posts 2
+ * Slug: ask-design-blog-child/ask-photo-blog-posts-2
+ * Categories: Posts
+ */
+?>
+<!-- wp:query {"queryId":72,"query":{"perPage":9,"pages":0,"offset":"13","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"metadata":{"patternName":"twentytwentyfive/template-query-loop-photo-blog","name":"Photo blog posts","description":"A list of posts, 3 columns, with only featured images.","categories":["query"]},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:query-no-results -->
+<!-- wp:paragraph {"style":{"typography":{"textAlign":"center"}}} -->
+<p class="has-text-align-center">Sorry, but nothing was found. Please try a search with different keywords.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"23rem"}} -->
+<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1"} /-->
+
+<!-- wp:post-title {"level":1,"isLink":true} /-->
+
+<!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}}} /-->
+
+<!-- wp:post-terms {"term":"category","style":{"typography":{"textTransform":"uppercase","letterSpacing":"1.4px"}}} /-->
+
+<!-- wp:post-excerpt {"fontSize":"medium"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:query-pagination {"paginationArrow":"arrow","align":"full","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:group --></div>
+<!-- /wp:query -->
