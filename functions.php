49f566d3afb218dbf1b8d0f979377cecb6e74633
Anne S. Katzeff <anne@askdesign.biz>
2026-08-07 14:16:20 -0400

    Fine tuning font awesome
    • specifying file path
    • tweaking the functions file
    • use specific css, not the all.css

--- functions.php
+++ functions.php
@@ -14,13 +14,27 @@
  * @since 1.0.0
  */
 
-function enqueue_local_fontawesome() {
-    wp_enqueue_style( 
-        'font-awesome-local', 
-        get_theme_file_uri( 'assets/fontawesome/css/all.min.css' ), 
-        array(), 
-        '7.x.x' 
+function enqueue_optimized_fontawesome_7() {
+    $fa_base_url = get_theme_file_uri( 'assets/fontawesome/css/' );
+
+    // 1. Mandatory Core File
+    wp_enqueue_style( 'fa-core', $fa_base_url . 'fontawesome.min.css', array(), '7.3.0' );
+
+    // 2. Individual Lean Styles matching your webfonts
+    $fa_styles = array(
+        'fa-brands'  => 'brands.min.css',
+        'fa-regular' => 'regular.min.css',
+        'fa-solid'   => 'solid.min.css'
     );
+
+    foreach ( $fa_styles as $handle => $filename ) {
+        wp_enqueue_style( 
+            $handle, 
+            $fa_base_url . $filename, 
+            array( 'fa-core' ), 
+            '7.3.0' 
+        );
+    }
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_local_fontawesome' );
-add_action( 'enqueue_block_editor_assets', 'enqueue_local_fontawesome' );
+add_action( 'wp_enqueue_scripts', 'enqueue_optimized_fontawesome_7' );
+add_action( 'enqueue_block_editor_assets', 'enqueue_optimized_fontawesome_7' );
